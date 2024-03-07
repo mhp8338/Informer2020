f6dcd5c85b5f0de495368c57f3bc54dabba9d1c0
mhp <mhp8338@qq.com>
2024-03-07 14:27:18 +0800

    update

--- models/model.py
+++ models/model.py
@@ -8,38 +8,65 @@ from models.decoder import Decoder, DecoderLayer
 from models.attn import FullAttention, ProbAttention, AttentionLayer
 from models.embed import DataEmbedding
 
+
 class Informer(nn.Module):
-    def __init__(self, enc_in, dec_in, c_out, seq_len, label_len, out_len, 
-                factor=5, d_model=512, n_heads=8, e_layers=3, d_layers=2, d_ff=512, 
-                dropout=0.0, attn='prob', embed='fixed', freq='h', activation='gelu', 
-                output_attention = False, distil=True, mix=True,
-                device=torch.device('cuda:0')):
+    def __init__(self, enc_in, dec_in, c_out, seq_len, label_len, pred_len,
+                 factor=5, d_model=512, n_heads=8, e_layers=3, d_layers=2, d_ff=512,
+                 dropout=0.0, attn='prob', embed='fixed', freq='h', activation='gelu',
+                 output_attention=False, distil=True, mix=True,
+                 device=torch.device('cuda:0')):
+        """
+        @param enc_in: Encoder input size (defaults to 7)
+        @param dec_in: Decoder input size (defaults to 7)
+        @param c_out: Output size (defaults to 7)
+        @param seq_len: Input sequence length of Informer encoder (defaults to 96)
+        @param label_len: Start token length of Informer decoder (defaults to 48)
+        @param out_len: Prediction sequence length (defaults to 24)
+        @param factor: Probsparse attn factor (defaults to 5)
+        @param d_model: Dimension of model (defaults to 512)
+        @param n_heads: Num of heads (defaults to 8)
+        @param e_layers: Num of encoder layers (defaults to 2)
+        @param d_layers: Num of decoder layers (defaults to 1)
+        @param d_ff: Dimension of fcn (defaults to 2048)
+        @param dropout: The probability of dropout (defaults to 0.05)
+        @param attn: Attention used in encoder (defaults to prob). This can be set to prob (informer), full (transformer)
+        @param embed: Time features encoding (defaults to timeF). This can be set to timeF, fixed, learned
+        @param freq: Freq for time features encoding (defaults to h). This can be set to s,t,h,d,b,w,m (s:secondly, t:minutely,
+        h:hourly, d:daily, b:business days, w:weekly, m:monthly).You can also use more detailed freq like 15min or 3h
+        @param activation: Activation function (defaults to gelu)
+        @param output_attention: Whether to output attention in encoder, using this argument means outputing attention (defaults to False)
+        @param distil: Whether to use distilling in encoder, using this argument means not using distilling (defaults to True)
+        @param mix: Whether to use mix attention in generative decoder, using this argument means not using mix attention (defaults to True)
+        @param device: Device ids of multile gpus (defaults to 0,1,2,3)
+        """
         super(Informer, self).__init__()
-        self.pred_len = out_len
+        self.pred_len = pred_len
         self.attn = attn
         self.output_attention = output_attention
 
         # Encoding
         self.enc_embedding = DataEmbedding(enc_in, d_model, embed, freq, dropout)
         self.dec_embedding = DataEmbedding(dec_in, d_model, embed, freq, dropout)
+
         # Attention
-        Attn = ProbAttention if attn=='prob' else FullAttention
+        Attn = ProbAttention if attn == 'prob' else FullAttention
+
         # Encoder
         self.encoder = Encoder(
             [
                 EncoderLayer(
-                    AttentionLayer(Attn(False, factor, attention_dropout=dropout, output_attention=output_attention), 
-                                d_model, n_heads, mix=False),
+                    AttentionLayer(Attn(False, factor, attention_dropout=dropout, output_attention=output_attention),
+                                   d_model, n_heads, mix=False),
                     d_model,
                     d_ff,
                     dropout=dropout,
                     activation=activation
-                ) for l in range(e_layers)
+                ) for _ in range(e_layers)
             ],
             [
                 ConvLayer(
                     d_model
-                ) for l in range(e_layers-1)
+                ) for _ in range(e_layers - 1)
             ] if distil else None,
             norm_layer=torch.nn.LayerNorm(d_model)
         )
@@ -47,24 +74,26 @@ class Informer(nn.Module):
         self.decoder = Decoder(
             [
                 DecoderLayer(
-                    AttentionLayer(Attn(True, factor, attention_dropout=dropout, output_attention=False), 
-                                d_model, n_heads, mix=mix),
-                    AttentionLayer(FullAttention(False, factor, attention_dropout=dropout, output_attention=False), 
-                                d_model, n_heads, mix=False),
+                    AttentionLayer(
+                        Attn(True, factor, attention_dropout=dropout, output_attention=False),
+                        d_model, n_heads, mix=mix),
+                    AttentionLayer(
+                        FullAttention(False, factor, attention_dropout=dropout, output_attention=False),
+                        d_model, n_heads, mix=False),
                     d_model,
                     d_ff,
                     dropout=dropout,
                     activation=activation,
                 )
-                for l in range(d_layers)
+                for _ in range(d_layers)
             ],
             norm_layer=torch.nn.LayerNorm(d_model)
         )
         # self.end_conv1 = nn.Conv1d(in_channels=label_len+out_len, out_channels=out_len, kernel_size=1, bias=True)
         # self.end_conv2 = nn.Conv1d(in_channels=d_model, out_channels=c_out, kernel_size=1, bias=True)
         self.projection = nn.Linear(d_model, c_out, bias=True)
-        
-    def forward(self, x_enc, x_mark_enc, x_dec, x_mark_dec, 
+
+    def forward(self, x_enc, x_mark_enc, x_dec, x_mark_dec,
                 enc_self_mask=None, dec_self_mask=None, dec_enc_mask=None):
         enc_out = self.enc_embedding(x_enc, x_mark_enc)
         enc_out, attns = self.encoder(enc_out, attn_mask=enc_self_mask)
@@ -72,21 +101,21 @@ class Informer(nn.Module):
         dec_out = self.dec_embedding(x_dec, x_mark_dec)
         dec_out = self.decoder(dec_out, enc_out, x_mask=dec_self_mask, cross_mask=dec_enc_mask)
         dec_out = self.projection(dec_out)
-        
+
         # dec_out = self.end_conv1(dec_out)
         # dec_out = self.end_conv2(dec_out.transpose(2,1)).transpose(1,2)
         if self.output_attention:
-            return dec_out[:,-self.pred_len:,:], attns
+            return dec_out[:, -self.pred_len:, :], attns
         else:
-            return dec_out[:,-self.pred_len:,:] # [B, L, D]
+            return dec_out[:, -self.pred_len:, :]  # [B, L, D]
 
 
 class InformerStack(nn.Module):
-    def __init__(self, enc_in, dec_in, c_out, seq_len, label_len, out_len, 
-                factor=5, d_model=512, n_heads=8, e_layers=[3,2,1], d_layers=2, d_ff=512, 
-                dropout=0.0, attn='prob', embed='fixed', freq='h', activation='gelu',
-                output_attention = False, distil=True, mix=True,
-                device=torch.device('cuda:0')):
+    def __init__(self, enc_in, dec_in, c_out, seq_len, label_len, out_len,
+                 factor=5, d_model=512, n_heads=8, e_layers=[3, 2, 1], d_layers=2, d_ff=512,
+                 dropout=0.0, attn='prob', embed='fixed', freq='h', activation='gelu',
+                 output_attention=False, distil=True, mix=True,
+                 device=torch.device('cuda:0')):
         super(InformerStack, self).__init__()
         self.pred_len = out_len
         self.attn = attn
@@ -96,16 +125,16 @@ class InformerStack(nn.Module):
         self.enc_embedding = DataEmbedding(enc_in, d_model, embed, freq, dropout)
         self.dec_embedding = DataEmbedding(dec_in, d_model, embed, freq, dropout)
         # Attention
-        Attn = ProbAttention if attn=='prob' else FullAttention
+        Attn = ProbAttention if attn == 'prob' else FullAttention
         # Encoder
 
-        inp_lens = list(range(len(e_layers))) # [0,1,2,...] you can customize here
+        inp_lens = list(range(len(e_layers)))  # [0,1,2,...] you can customize here
         encoders = [
             Encoder(
                 [
                     EncoderLayer(
-                        AttentionLayer(Attn(False, factor, attention_dropout=dropout, output_attention=output_attention), 
-                                    d_model, n_heads, mix=False),
+                        AttentionLayer(Attn(False, factor, attention_dropout=dropout, output_attention=output_attention),
+                                       d_model, n_heads, mix=False),
                         d_model,
                         d_ff,
                         dropout=dropout,
@@ -115,7 +144,7 @@ class InformerStack(nn.Module):
                 [
                     ConvLayer(
                         d_model
-                    ) for l in range(el-1)
+                    ) for l in range(el - 1)
                 ] if distil else None,
                 norm_layer=torch.nn.LayerNorm(d_model)
             ) for el in e_layers]
@@ -124,10 +153,10 @@ class InformerStack(nn.Module):
         self.decoder = Decoder(
             [
                 DecoderLayer(
-                    AttentionLayer(Attn(True, factor, attention_dropout=dropout, output_attention=False), 
-                                d_model, n_heads, mix=mix),
-                    AttentionLayer(FullAttention(False, factor, attention_dropout=dropout, output_attention=False), 
-                                d_model, n_heads, mix=False),
+                    AttentionLayer(Attn(True, factor, attention_dropout=dropout, output_attention=False),
+                                   d_model, n_heads, mix=mix),
+                    AttentionLayer(FullAttention(False, factor, attention_dropout=dropout, output_attention=False),
+                                   d_model, n_heads, mix=False),
                     d_model,
                     d_ff,
                     dropout=dropout,
@@ -140,8 +169,8 @@ class InformerStack(nn.Module):
         # self.end_conv1 = nn.Conv1d(in_channels=label_len+out_len, out_channels=out_len, kernel_size=1, bias=True)
         # self.end_conv2 = nn.Conv1d(in_channels=d_model, out_channels=c_out, kernel_size=1, bias=True)
         self.projection = nn.Linear(d_model, c_out, bias=True)
-        
-    def forward(self, x_enc, x_mark_enc, x_dec, x_mark_dec, 
+
+    def forward(self, x_enc, x_mark_enc, x_dec, x_mark_dec,
                 enc_self_mask=None, dec_self_mask=None, dec_enc_mask=None):
         enc_out = self.enc_embedding(x_enc, x_mark_enc)
         enc_out, attns = self.encoder(enc_out, attn_mask=enc_self_mask)
@@ -149,10 +178,10 @@ class InformerStack(nn.Module):
         dec_out = self.dec_embedding(x_dec, x_mark_dec)
         dec_out = self.decoder(dec_out, enc_out, x_mask=dec_self_mask, cross_mask=dec_enc_mask)
         dec_out = self.projection(dec_out)
-        
+
         # dec_out = self.end_conv1(dec_out)
         # dec_out = self.end_conv2(dec_out.transpose(2,1)).transpose(1,2)
         if self.output_attention:
-            return dec_out[:,-self.pred_len:,:], attns
+            return dec_out[:, -self.pred_len:, :], attns
         else:
-            return dec_out[:,-self.pred_len:,:] # [B, L, D]
+            return dec_out[:, -self.pred_len:, :]  # [B, L, D]
